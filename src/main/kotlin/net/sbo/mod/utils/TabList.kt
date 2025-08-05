d47aa0a88226712401a1c5ec9b297540b55e0b03
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2025-08-05 16:12:22 +0200

    stuff

---
+++ src/main/kotlin/net/sbo/mod/utils/TabList.kt
@@ -0,0 +1,14 @@
+package net.sbo.mod.utils
+
+import net.minecraft.client.network.PlayerListEntry
+
+object TabList {
+    /**
+     * Returns a list of all PlayerListEntry objects from the current tab list.
+     * Each PlayerListEntry object contains detailed information about a player.
+     */
+    fun getTabEntries(): List<PlayerListEntry> {
+        val client = net.minecraft.client.MinecraftClient.getInstance()
+        return client.player?.networkHandler?.playerList ?: emptyList()
+    }
+}
