2c2624c4166c50ff9f3a0aaee926254ba05702c2
RolexDE <133770982+RolexDE@users.noreply.github.com>
2025-08-12 16:39:42 +0200

    fix: Update chat message handling for party invitations and improve request acceptance logic

--- src/main/kotlin/net/sbo/mod/utils/ChatHandler.kt
+++ src/main/kotlin/net/sbo/mod/utils/ChatHandler.kt
@@ -1,5 +1,6 @@
 package net.sbo.mod.utils
 
+import gg.essential.universal.utils.toFormattedString
 import net.minecraft.text.Text
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -13,7 +14,7 @@ object ChatHandler {
     }
 
     fun processMessage(message: Text): Boolean {
-        val messageString = message.string
+        val messageString = message.toFormattedString()
         messageHandlers.forEach { rule ->
             val matcher = rule.pattern.matcher(messageString)
             if (matcher.find()) {
