3bdaab7dcf293804d09690a8b1fad072b5c375f2
RolexDE <133770982+RolexDE@users.noreply.github.com>
2025-08-24 18:45:01 +0200

    feat: Ensure minimum playtime for burrow calculations

--- src/main/kotlin/net/sbo/mod/overlays/DianaLoot.kt
+++ src/main/kotlin/net/sbo/mod/overlays/DianaLoot.kt
@@ -109,7 +109,8 @@ object DianaLoot {
         val chimLsPercent = calcPercentOne(tracker.items, tracker.mobs, "CHIMERA_LS", "MINOS_INQUISITOR_LS")
         val relicPercent = calcPercentOne(tracker.items, tracker.mobs, "MINOS_RELIC", "MINOS_CHAMPION")
         val stickPercent = calcPercentOne(tracker.items, tracker.mobs, "DAEDALUS_STICK", "MINOTAUR")
-        val playTimeHrs = tracker.items.TIME.toDouble() / TimeUnit.HOURS.toMillis(1)
+        var playTimeHrs = tracker.items.TIME.toDouble() / TimeUnit.HOURS.toMillis(1)
+        if (playTimeHrs < 0.01) playTimeHrs = 0.01
         val burrowsPerHr = if (tracker.items.TOTAL_BURROWS > 0) {
             BigDecimal(tracker.items.TOTAL_BURROWS.toDouble() / playTimeHrs).setScale(2, RoundingMode.HALF_UP).toDouble()
         } else 0.0
